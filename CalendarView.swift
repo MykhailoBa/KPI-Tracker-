e4ee8fadcb4001117212479424c9f2e12efb2d37
MykhailoBa <misha@babych.info>
2025-02-12 17:55:55 +0000

    Create CalendarView.swift

---
+++ CalendarView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+struct CalendarView: View {
+    var body: some View {
+        VStack {
+            Text("Календар змін")
+                .font(.largeTitle)
+                .padding()
+            
+            Spacer()
+        }
+        .navigationTitle("Календар")
+    }
+}
