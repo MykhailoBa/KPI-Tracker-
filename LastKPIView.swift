5f2cd7edfb1afd7bcf8ae5e5e7848eedf8795586
MykhailoBa <misha@babych.info>
2025-02-12 17:54:33 +0000

    Create LastKPIView.swift

---
+++ LastKPIView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+struct LastKPIView: View {
+    var body: some View {
+        VStack {
+            Text("Останній KPI: 91.6%")
+                .font(.largeTitle)
+                .padding()
+            
+            Spacer()
+        }
+        .navigationTitle("Останній KPI")
+    }
+}
